7d5b750183fd7488883deba14ba06072744e22b0
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-10-20 09:59:52 +0300

    Refactor SpringConfig.java, MySpringMvcDis...java. Delete xml config

--- src/main/java/ru/netology/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/ru/netology/config/MySpringMvcDispatcherServletInitializer.java
@@ -4,19 +4,29 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 // За счет данного класс убираем файл web.xml, и всю реализацию Spring mvc через xml
 public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    // Данный метод мы использовать не будем, возвращаем null
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return null;
     }
 
     // здесь укажем класс конфигурации, который будет заменять applicationContextMVC.xml
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        // Подставляем класс с конфигурацией
+        return new Class[]{SpringConfig.class};
     }
 
+
+    // в данном методе возвращаем /
+    // как в файле xml посылали на диспетчер сервлет <servlet-mapping>
+    //        <servlet-name>dispatcher</servlet-name>
+    //        <url-pattern>/</url-pattern>
+    //    </servlet-mapping>
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+//        посылаем на /, все запросы от пользователя посылаем на dispatcher servlet
+        return new String[]{"/"};
     }
 }
--- src/main/java/ru/netology/config/SpringConfig.java
+++ src/main/java/ru/netology/config/SpringConfig.java
@@ -1,8 +1,16 @@
 package ru.netology.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 // это конфигурационный java класс,
 @Configuration
@@ -10,7 +18,47 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan("ru.netology")
 @EnableWebMvc // так как наше приложение поддерживает web функции,
 // данная аннотация равносильна тегу <mvc:annotation-driven/>, который находился в applicationContextMVC.xml
-public class SpringConfig {
+public class SpringConfig implements WebMvcConfigurer {
     // Осталось реализовать бины которые отвечали за конфигурацию шаблонизатора time live.
+
+    // С помощью Spring и аннотации @Autowired, создаем ApplicationContext, его используем в бине templateResolver
+    // Что бы настроить timeLeaf
+    private final ApplicationContext applicationContext;
+
+    @Autowired
+    public SpringConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        // задаем папку где будут лежать наши представления
+        templateResolver.setPrefix("/WEB-INF/views/");
+        // задаем расширение этих представлений
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+
+    // В данном бине так же задаем конфигурацию наших представлений
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    // Так как мы реализуем интерфейс WebMvcConfigurer, с этим интерфейсом реализовываем метод configureViewResolvers
+    // Этот интерфейс реализуется, когда мы хотим настроить под себя Spring MVC,
+    // Задаем шаблонизатор ThymeleafViewResolver
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        registry.viewResolver(resolver);
+    }
 
 }
