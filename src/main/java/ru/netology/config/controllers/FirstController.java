5149551dfbae16fe2bd456f05920aabfdef550b5
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-10-26 14:39:52 +0300

    Initial commit

--- src/main/java/ru/netology/config/controllers/FirstController.java
+++ src/main/java/ru/netology/config/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.netology.config.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,23 +14,12 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("/first")
 public class FirstController {
 
-    // для приема HttpServletRequest - этот объект будет внедрен Spring, и в этом объекте содержатся все сведения о
-    // поступающем http запросе от пользователя
-//
-//    public String helloPage(HttpServletRequest request) {
-//
-//        // в url пользователь должен будет передавать 2 параметра, имя и фамилия.
-//        // Параметр name, будет иметь ключ name.
-//        String name = request.getParameter("name");
-//        // Параметр surname, будет иметь ключ surname.
-//        String surname = request.getParameter("surname");
 
-
-    //    required = false дает возможность работать без параметров, но будет null
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello, " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname, Model model) {
+        // System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
         return "first/hello";
     }
 
